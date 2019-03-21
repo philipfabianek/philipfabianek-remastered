7fbfbbfe269684efc9ea527b432cc427eb5a0bd7
Filip Fabiánek <fabianekphilip@gmail.com>
2019-03-21 21:39:28 +0100

    Add prop-types, add some LandingPage content, add a lot of styles, make Head reusable

---
+++ components/reusable/Head.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import PropTypes from 'prop-types';
+
+const HeadComponent = (props) => (
+  <Head>
+    <link key="apple-touch-icon" rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+    <link key="icon-32x32" rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+    <link key="icon-16x16" rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+    <link key="manifest" rel="manifest" href="/static/favicon/site.webmanifest" />
+    <link key="mask-icon" rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#3d3d3d" />
+    <link key="shortcut-icon" rel="shortcut icon" href="/static/favicon/favicon.ico" />
+    <meta key="msapplication-TileColor" name="msapplication-TileColor" content="#da532c" />
+    <meta key="theme-color" name="theme-color" content="#ffffff" />
+
+    <meta key="charset" charset="UTF-8" />
+    <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta key="http-equiv" http-equiv="X-UA-Compatible" content="ie=edge" />
+
+    {/* Raleway */}<link href="//fonts.googleapis.com/css?family=Raleway:300,400,600&subset=latin,latin-ext" rel="stylesheet" />
+    {/* Roboto */}<link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,600&subset=latin,latin-ext" rel="stylesheet" />
+    {/* Passion One */}<link href="//fonts.googleapis.com/css?family=Passion+One:400,700,900&subset=latin,latin-ext" rel="stylesheet" />
+    {/* Lato */}<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900&subset=latin,latin-ext" rel="stylesheet" />
+
+    <meta key="description" name="description" content={props.description} />
+    <title>{props.title}</title>
+
+    {props.children}
+  </Head>
+);
+
+HeadComponent.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
+
+export default HeadComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 // Next.js
-import Head from "next/head";
+import Link from 'next/link'
+
+// Head
+import { Head } from "../components/reusable";
 
 // Styles
 import "../styles"
@@ -7,27 +10,35 @@ import "../styles"
 const IndexPage = () => {
   return (
     <div>
-      <Head>
-        <link key="apple-touch-icon" rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
-        <link key="icon-32x32" rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-        <link key="icon-16x16" rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
-        <link key="manifest" rel="manifest" href="/static/favicon/site.webmanifest" />
-        <link key="mask-icon" rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#3d3d3d" />
-        <link key="shortcut-icon" rel="shortcut icon" href="/static/favicon/favicon.ico" />
-        <meta key="msapplication-TileColor" name="msapplication-TileColor" content="#da532c" />
-        <meta key="theme-color" name="theme-color" content="#ffffff" />
+      <Head
+        title="Welcome | Philip Fabianek - Web Developer"
+        description="Full-Stack Web Developer that produces high-end websites and applications that focus on simplicity and the best possible user experience."
+      />
 
-        <meta key="charset" charset="UTF-8" />
-        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta key="http-equiv" http-equiv="X-UA-Compatible" content="ie=edge" />
+      <div className="landing-page">
+        <div className="landing-page__initial-content">
+          <div className="landing-page__rectangle"></div>
 
-        <meta key="description" name="description" content="Full-Stack Web Developer that produces high-end websites and applications that focus on simplicity and the best possible user experience." />
-        <title>Welcome | Philip Fabianek - Web Developer</title>
-      </Head>
-      <div className="welcome-page">
-        <h2 className="welcome-page__title">
-          Welcome to Next.js!
-        </h2>
+          <div className="landing-page__circles"></div>
+
+          <div className="landing-page__text">
+            <h1 className="landing-page__title">
+              Philip&nbsp;Fabianek
+            </h1>
+
+            <h2 className="landing-page__subtitle">
+              is&nbsp;a&nbsp;Web&nbsp;Developer
+            </h2>
+          </div>
+
+          <button className="landing-page__button">
+            Explore
+          </button>
+        </div>
+
+        <div className="landing-page__selection">
+
+        </div>
       </div>
     </div>
   );
