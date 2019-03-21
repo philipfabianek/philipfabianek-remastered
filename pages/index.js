18f2ddb7d60a3c754bb5538aa99ca6b53c148c5e
Filip Fabiánek <fabianekphilip@gmail.com>
2019-03-21 20:12:05 +0100

    Add image and adjust styles

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@
 import "../styles"
 
 const Home = () => {
-  return <div>Welcome to Next.js!</div>
-}
+  return (
+    <div className="welcome-page">
+      <h2 className="welcome-page__title">
+        Welcome to Next.js!
+      </h2>
+    </div>
+  );
+};
 
 export default Home
