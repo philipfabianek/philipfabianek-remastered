e3b96bebee796e907cdfb301efe0dac487b4849f
Filip Fabiánek <fabianekphilip@gmail.com>
2019-06-15 21:27:00 +0200

    Allow the SquareTransition to be reversed (vertically flipped)

--- components/transitions/SquareTransition.js
+++ components/transitions/SquareTransition.js
@@ -1,3 +1,21 @@
-export default () => (
-  <div className='square-transition' />
-);
+// React
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const SquareTransition = ({ reversed = false }) => {
+  let className = 'square-transition';
+
+  if (reversed) {
+    className += ' square-transition--reversed';
+  }
+
+  return (
+    <div className={className} />
+  );
+};
+
+SquareTransition.propTypes = {
+  reversed: PropTypes.bool,
+};
+
+export default SquareTransition;
